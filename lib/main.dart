08f85ea57b42e2c0b88aa7acdf139b64b10e52a8
matthew colson <matthew.p.colson@gmail.com>
2021-01-10 13:52:58 -0600

    game working.

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,182 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var coins = 100;
+  var slotOne = 0;
+  var slotTwo = 0;
+  var slotThree = 0;
+  var title = 'Spin to win';
+  var coinsColor = Colors.white;
+  var titleColor = Colors.lightBlueAccent;
+  var random = Random();
+
+  void spin() {
+    coins -= 5;
+    slotOne = random.nextInt(10);
+    slotTwo = random.nextInt(10);
+    slotThree = random.nextInt(10);
+  }
+
+  void score() {
+    if (slotOne == 7 && slotTwo == 7 && slotThree == 7) {
+      coins += 1000;
+      title = 'WINNER 1000 COINS !!!!';
+      coinsColor = Colors.greenAccent;
+      titleColor = Colors.greenAccent;
+    } else if (slotOne == slotTwo && slotOne == slotThree) {
+      coins += 100;
+      title = 'WINNER 100 COINS !!!!';
+      coinsColor = Colors.greenAccent;
+      titleColor = Colors.greenAccent;
+    } else if (slotOne == slotTwo ||
+        slotOne == slotThree ||
+        slotTwo == slotThree) {
+      coins += 10;
+      title = 'WINNER 10 COINS !!!!';
+      coinsColor = Colors.greenAccent;
+      titleColor = Colors.greenAccent;
+    } else {
+      title = 'Sorry, spin again';
+      coinsColor = Colors.redAccent;
+      titleColor = Colors.redAccent;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey.shade900,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            'Slot Game',
+            style: TextStyle(
+              color: Colors.yellowAccent,
+              fontSize: 20.0,
+              fontFamily: 'SourceSansPro',
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+            ),
+          ),
+          actions: [
             Text(
-              'You have pushed the button this many times:',
+              'Coins:',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontFamily: 'SourceSansPro',
+                fontWeight: FontWeight.normal,
+                letterSpacing: 1.0,
+              ),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              coins.toString(),
+              style: TextStyle(
+                color: coinsColor,
+                fontSize: 20.0,
+                fontFamily: 'SourceSansPro',
+                fontWeight: FontWeight.normal,
+                letterSpacing: 1.0,
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                color: titleColor,
+                fontFamily: 'SourceSansPro',
+                fontSize: 20,
+                fontWeight: FontWeight.normal,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                      left: 10.0, right: 5.0, top: 20.0, bottom: 10.0),
+                  color: Colors.white,
+                  child: Text(
+                    slotOne.toString(),
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 100,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                      left: 10.0, right: 5.0, top: 20.0, bottom: 10.0),
+                  color: Colors.white,
+                  child: Text(
+                    slotTwo.toString(),
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 100,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                      left: 10.0, right: 5.0, top: 20.0, bottom: 10.0),
+                  color: Colors.white,
+                  child: Text(
+                    slotThree.toString(),
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 100,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            FlatButton(
+              color: Colors.grey,
+              child: Text(
+                'SPIN',
+                style: TextStyle(
+                  color: Colors.yellowAccent,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  spin();
+                  score();
+                });
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
